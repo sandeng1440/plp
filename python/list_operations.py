45c5ee881e9da7ba635ce39d09f9e47309f4ce87
sandeng1440 <sandeng1440@gmail.com>
2024-02-23 22:10:01 +0300

    adding python/list_operations.py

---
+++ python/list_operations.py
@@ -0,0 +1,24 @@
+# making an empty list
+my_list = []
+
+# adding 10, 20, 30, 40 to the end to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# inserting 15 in the second position
+my_list.insert(1,15)
+
+# merging two lists
+my_list.extend([50,60,70])
+
+# removing the last element
+my_list.pop()
+
+# sorting in ascending order
+my_list.sort()
+
+# getting the index of 38
+print(f"The index of 30: {my_list.index(30)}")
+
