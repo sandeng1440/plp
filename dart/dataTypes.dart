10c6c3a5ba328163398b4687e82271228bc3835a
sandeng1440 <sandeng1440@gmail.com>
2024-03-02 11:48:59 +0300

    Added dart/dataTypes.dart

---
+++ dart/dataTypes.dart
@@ -0,0 +1,30 @@
+
+int main(){
+  // Integer: Used to store numbers of integer type
+  int numInt = 45;
+
+  // Double: Used to store numbers with decimal points
+  double numInt = 45.23;
+  
+  // String: Used to store a collection of characters
+  String text = "This is a String";
+
+  // String with runes: Runes are UTF-16 numbers that are codes for emojis
+  String textEmojis = "Emojis: \u{1F453} \u{1F536}";
+
+  // List of strings: Used to store a collection of variables of the same type
+  List<String> textList = ["This", "is", "a", "List", "of", "Strings"];
+
+  // Map of <string: int>: Used to store key,value pairs
+  Map<String: int> mapOfString = {
+    "One": 1,
+    "Two": 2,
+    "Three": 3,
+    "Four": 4,
+    }
+  
+  // Null: Used to store a lack of values
+  var valNull = Null;
+
+  return 0;
+}
