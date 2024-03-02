05560699ddb5291ded01591b79bd7a407d10fd32
sandeng1440 <sandeng1440@gmail.com>
2024-03-02 12:57:40 +0300

    Changes to dart/functions.dart

--- dart/functions.dart
+++ dart/functions.dart
@@ -18,14 +18,19 @@ int stringLength(String a) => a.length;
 Object getFirstElement(List<Object> a) => a[0];
 
 int main(){
+  // Lists of all types to test the getFirstElement function with
   List<String> strList = ["List", "Of", "Strings"];
   List<int> intList = [4,5,2,7,8,2];
   List<double> doubleList = [53.4, 2.7,63,7.8742];
   List<bool> boolList = [false, true, false, false, true, true];
 
+  // doubles to test the arithmetic funtions
   double num1 = 35.2, num2 = 60.4;
+
+  // String to test the stringLength function
   String str = "The length of this string";
 
+  // Tests 
   print("strList[0]: ${getFirstElement(strList)}");
   print("intList[0]: ${getFirstElement(intList)}");
   print("doubleList[0]: ${getFirstElement(doubleList)}");
