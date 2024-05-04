56a8580d669d626e40efe24a2d8b7b67db550727
sandeng1440 <sandeng1440@gmail.com>
2024-05-04 15:59:59 +0300

    Reinstalled the venv

--- python/django-projects/plp_store/plp_store/plp_store/urls.py
+++ python/django-projects/plp_store/plp_store/plp_store/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for web_store project.
+URL configuration for plp_store project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
