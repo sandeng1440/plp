d06c2e583f7c2e8c4d0e79798009539b1f87ec51
sandeng1440 <sandeng1440@gmail.com>
2024-05-06 13:08:42 +0300

    Auto commit

--- python/django-projects/poll_site/poll_site/polls/views.py
+++ python/django-projects/poll_site/poll_site/polls/views.py
@@ -1,8 +1,10 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
+
 from .models import Question, Choice
 
 
@@ -13,22 +15,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
-def index(request):
-    latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
